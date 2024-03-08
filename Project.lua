7b7d6d307b92db2703d47eb37ed297a49e933ca8
Torsten Keßler <t.kessler@posteo.de>
2024-03-08 12:43:56 +0100

    <release> complex..v0.2.0

--- Project.lua
+++ Project.lua
@@ -1,8 +1,9 @@
 Project = {
-    name = "complex",
-    uuid = "b8b2a887-2064-4f29-8f96-afd201c4c64f",
-    authors = {"Torsten Keßler<t.kessler@posteo.de>"},
-    version = "0.1.0",
-    deps = {}
+  name = "complex",
+  uuid = "b8b2a887-2064-4f29-8f96-afd201c4c64f",
+  authors = {"Torsten Keßler<t.kessler@posteo.de>", }, 
+  version = "0.2.0",
+  deps = {
+    },
 }
 return Project
